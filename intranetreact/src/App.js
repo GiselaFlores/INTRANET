fd3fdaed594fa9a3446329b903ef560732999a38
GiselaFlores <gi.flores@bue.edu.ar>
2022-08-17 22:19:36 -0300

    caratula inicial

---
+++ intranetreact/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          BIENVENID@S A REACT
+        </p>
+        <a
+          className="App-link"
+          href="https://aulasvirtuales.bue.edu.ar/login/index.php"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          AULA VIRTUAL
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
